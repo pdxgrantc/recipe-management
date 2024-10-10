06f6b9df3030725893194ff6fdca5dd2fd310104
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-10-09 17:37:48 -0700

    basic edit mode

--- src/pages/CreateRecipe/CreateRecipe.jsx
+++ src/pages/CreateRecipe/CreateRecipe.jsx
@@ -75,54 +75,58 @@ function CreateForm() {
             <textarea name="Description" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
             <div className='flex flex-col gap-2'>
                 <SubTitle text='Ingredients' />
-                <ul>
-                    {ingredients.map((ingredient, index) => (
-                        <li key={index} className='flex items-center gap-2'>
-                            <input name="Ingredient Amount" type="number" placeholder="Amount" value={ingredient.amount} onChange={(e) => {
-                                const newIngredients = [...ingredients];
-                                newIngredients[index].amount = e.target.value;
-                                setIngredients(newIngredients);
-                            }} />
-                            <select name="Ingredient Measurement Type">
-                                <option value="unit">Unit</option>
-                                <option value="cup">Cup</option>
-                                <option value="tbsp">Tbsp</option>
-                                <option value="tsp">Tsp</option>
-                                <option value="fluid-oz">Fluid oz</option>
-                                <option value="g">G</option>
-                                <option value="kg">Kg</option>
-                                <option value="lb">Lb</option>
-                                <option value='ml'>Ml</option>
-                            </select>
-                            <input name="Ingredient Name" type="text" placeholder="Ingredient" value={ingredient.ingredient} onChange={(e) => {
-                                const newIngredients = [...ingredients];
-                                newIngredients[index].ingredient = e.target.value;
-                                setIngredients(newIngredients);
-                            }} />
-                            <button className='icon-button'>
-                                <DeleteIcon className='w-8 h-8' onClick={() => handleDeleteIngredient(index)} />
-                            </button>
-                        </li>
-                    ))}
-                </ul>
+                {ingredients.length !== 0 &&
+                    <ul>
+                        {ingredients.map((ingredient, index) => (
+                            <li key={index} className='flex items-center gap-2'>
+                                <input name="Ingredient Amount" type="number" placeholder="Amount" value={ingredient.amount} onChange={(e) => {
+                                    const newIngredients = [...ingredients];
+                                    newIngredients[index].amount = e.target.value;
+                                    setIngredients(newIngredients);
+                                }} />
+                                <select name="Ingredient Measurement Type">
+                                    <option value="unit">Unit</option>
+                                    <option value="cup">Cup</option>
+                                    <option value="tbsp">Tbsp</option>
+                                    <option value="tsp">Tsp</option>
+                                    <option value="fluid-oz">Fluid oz</option>
+                                    <option value="g">G</option>
+                                    <option value="kg">Kg</option>
+                                    <option value="lb">Lb</option>
+                                    <option value='ml'>Ml</option>
+                                </select>
+                                <input name="Ingredient Name" type="text" placeholder="Ingredient" value={ingredient.ingredient} onChange={(e) => {
+                                    const newIngredients = [...ingredients];
+                                    newIngredients[index].ingredient = e.target.value;
+                                    setIngredients(newIngredients);
+                                }} />
+                                <button className='icon-button'>
+                                    <DeleteIcon className='w-8 h-8' onClick={() => handleDeleteIngredient(index)} />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                }
                 <button onClick={handleAddIngredient} className='text-button page-button'>Add Ingredient</button>
             </div>
             <div className='flex flex-col gap-2'>
                 <SubTitle text='Instructions' />
-                <ul>
-                    {steps.map((step, index) => (
-                        <li key={index}>
-                            <input name="Step Text" type="text" value={step} onChange={(e) => {
-                                const newSteps = [...steps];
-                                newSteps[index] = e.target.value;
-                                setSteps(newSteps);
-                            }} />
-                            <button className='icon-button'>
-                                <DeleteIcon className='w-8 h-8' onClick={() => handleDeleteStep(index)} />
-                            </button>
-                        </li>
-                    ))}
-                </ul>
+                {steps.length !== 0 &&
+                    <ul>
+                        {steps.map((step, index) => (
+                            <li key={index} className='flex items-center gap-2'>
+                                <input name="Step Text" type="text" value={step} onChange={(e) => {
+                                    const newSteps = [...steps];
+                                    newSteps[index] = e.target.value;
+                                    setSteps(newSteps);
+                                }} />
+                                <button className='icon-button'>
+                                    <DeleteIcon className='w-8 h-8' onClick={() => handleDeleteStep(index)} />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                }
                 <button onClick={handleAddStep} className='text-button page-button'>Add Step</button>
             </div>
             <div>
--- src/pages/Recipe/EditRecipe.jsx
+++ src/pages/Recipe/EditRecipe.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Firebase
 import { auth } from '../../firebase';
@@ -11,17 +11,139 @@ import { MdOutlineCancel as CancelIcon } from "react-icons/md";
 
 
 // Utils
-import { SwitchButton } from '../../assets/Utils';
+import { SwitchButton, SubTitle } from '../../assets/Utils';
 
 
 export default function EditRecipe({ recipe, setEditing }) {
     const [user] = useAuthState(auth);
+
+    const [title, setTitle] = useState(recipe.title);
+    const [description, setDescription] = useState(recipe.description);
+    const [ingredients, setIngredients] = useState(recipe.ingredients);
+    const [steps, setSteps] = useState(recipe.steps);
+
+    const handleAddIngredient = () => {
+        setIngredients([...ingredients, { amount: '', unit: '', ingredient: '' }]);
+    }
+
+    const handleDeleteIngredient = (index) => {
+        const newIngredients = [...ingredients];
+        newIngredients.splice(index, 1);
+        setIngredients(newIngredients
+        );
+    }
+
+    const handleAddStep = () => {
+        setSteps([...steps, '']);
+    }
+
+    const handleDeleteStep = (index) => {
+        const newSteps = [...steps];
+        newSteps.splice(index, 1);
+        setSteps(newSteps);
+    }
+
+    const handleSubmit = async () => {
+        console.log('submitting');
+
+    }
 
     const handleEdit = () => {
         setEditing(false);
     }
 
     return (
+        <>
+            <div className='flex gap-5'>
+                <div className='flex flex-col gap-2'>
+                    <input name="Recipe Title" type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                    <textarea name="Description" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                </div>
+                <div className='flex flex-col'>
+                    <SwitchButton text='Share' onClick={() => { }} />
+                    <button name='Cancel' className='text-button page-button'>
+                        <p>Cancel</p>
+                        <CancelIcon />
+                    </button>
+                    <button name='delete recipe' className='text-button page-button'>
+                        <p>Delete</p>
+                        <DeleteIcon />
+                    </button>
+                    <button name='toggle edit' className='text-button page-button' onClick={handleEdit}>
+                        <p>Save</p>
+                        <SaveIcon />
+                    </button>
+                </div>
+            </div>
+            <div className='flex flex-col gap-2'>
+                <SubTitle text='Ingredients' />
+                {ingredients.length !== 0 &&
+                    <ul>
+                        {ingredients.map((ingredient, index) => (
+                            <li key={index} className='flex items-center gap-2'>
+                                <input name="Ingredient Amount" type="number" placeholder="Amount" value={ingredient.amount} onChange={(e) => {
+                                    const newIngredients = [...ingredients];
+                                    newIngredients[index].amount = e.target.value;
+                                    setIngredients(newIngredients);
+                                }} />
+                                <select name="Ingredient Measurement Type">
+                                    <option value="unit">Unit</option>
+                                    <option value="cup">Cup</option>
+                                    <option value="tbsp">Tbsp</option>
+                                    <option value="tsp">Tsp</option>
+                                    <option value="fluid-oz">Fluid oz</option>
+                                    <option value="g">G</option>
+                                    <option value="kg">Kg</option>
+                                    <option value="lb">Lb</option>
+                                    <option value='ml'>Ml</option>
+                                </select>
+                                <input name="Ingredient Name" type="text" placeholder="Ingredient" value={ingredient.ingredient} onChange={(e) => {
+                                    const newIngredients = [...ingredients];
+                                    newIngredients[index].ingredient = e.target.value;
+                                    setIngredients(newIngredients);
+                                }} />
+                                <button className='icon-button'>
+                                    <DeleteIcon className='w-8 h-8' onClick={() => handleDeleteIngredient(index)} />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                }
+                <button onClick={handleAddIngredient} className='text-button page-button'>Add Ingredient</button>
+            </div>
+            <div className='flex flex-col gap-2'>
+                <SubTitle text='Instructions' />
+                {steps.length !== 0 &&
+                    <ul>
+                        {steps.map((step, index) => (
+                            <li key={index} className='flex items-center gap-2'>
+                                <input name="Step Text" type="text" value={step} onChange={(e) => {
+                                    const newSteps = [...steps];
+                                    newSteps[index] = e.target.value;
+                                    setSteps(newSteps);
+                                }} />
+                                <button className='icon-button'>
+                                    <DeleteIcon className='w-8 h-8' onClick={() => handleDeleteStep(index)} />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                }
+                <button onClick={handleAddStep} className='text-button page-button'>Add Step</button>
+            </div>
+            <div>
+                <button onClick={handleSubmit} className='text-button page-button'>Submit</button>
+            </div>
+        </>
+    )
+}
+
+
+
+
+
+/*
+return (
         <div>
             {recipe && (
                 <div>
@@ -51,6 +173,7 @@ export default function EditRecipe({ recipe, setEditing }) {
         </div>
     )
 }
+*/
 
 function Steps({ steps }) {
     return (
