83b71a96d1aaadb4e4adf8dc366042c02f6047d8
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-10-14 20:11:24 -0700

    updated my recent recipes to pull first image in firebase

--- src/assets/MyRecentRecipes.jsx
+++ src/assets/MyRecentRecipes.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
 // Firebase
-import { auth, db } from '../firebase';
+import { auth, db, storage } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { collection, query, orderBy, limit, onSnapshot } from 'firebase/firestore';
+import { ref, listAll, getDownloadURL } from 'firebase/storage';
 
 // Utils
 import MyRecipeList from './MyRecipeList';
@@ -12,7 +13,9 @@ import MyRecipeList from './MyRecipeList';
 export default function MyRecentRecipes({ numRecipes }) {
     const [user] = useAuthState(auth);
     const [recipes, setRecipes] = useState([]);
+    const [recipesWithThumbnails, setRecipesWithThumbnails] = useState([]);
 
+    // Fetch input number of recipes from Firestore
     useEffect(() => {
         if (user) {
             const recipesRef = collection(db, 'users', user.uid, 'recipes');
@@ -29,10 +32,34 @@ export default function MyRecentRecipes({ numRecipes }) {
             // Cleanup subscription on unmount
             return () => unsubscribe();
         }
-    }, [user]);
+    }, [user, numRecipes]);
+
+    // Fetch the thumbnails of the recipes
+    useEffect(() => {
+        const fetchThumbnails = async () => {
+            const updatedRecipes = await Promise.all(recipes.map(async (recipe) => {
+                try {
+                    const imagesRef = ref(storage, `users/${user.uid}/recipes/${recipe.id}`);
+                    const result = await listAll(imagesRef);
+                    if (result.items.length > 0) {
+                        const imageUrl = await getDownloadURL(result.items[0]);
+                        return { ...recipe, thumbnail: imageUrl };
+                    }
+                    return recipe;
+                } catch (error) {
+                    console.error('Error fetching image URL:', error);
+                    return recipe;
+                }
+            }));
+            setRecipesWithThumbnails(updatedRecipes);
+        };
+
+        if (recipes.length > 0) {
+            fetchThumbnails();
+        }
+    }, [recipes, user]);
 
     return (
-        <MyRecipeList recipes={recipes} />
+        <MyRecipeList recipes={recipesWithThumbnails} />
     );
-
 }
